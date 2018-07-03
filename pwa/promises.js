dcda5d4633d1799b7b13bd898fb72cb96b485f66
Christian <jcromerohdz@gmail.com>
2018-07-03 14:54:15 -0700

    JS Promises stand alone

--- pwa/promises.js
+++ pwa/promises.js
@@ -1,20 +1,32 @@
 //JS Promises
 
-function addExtra(price) {
-    return new Promise(function(resolve, reject){
-        if (price > 2) reject("Price cannot exceed 3");
+// function addExtra(price) {
+//     return new Promise(function(resolve, reject){
+//         if (price > 2) reject("Price cannot exceed 3");
 
-        setTimeout(function(){
-           resolve(price + 1); 
-        }, 300);
-    });
-}
+//         setTimeout(function(){
+//            resolve(price + 1); 
+//         }, 300);
+//     });
+// }
 
-addExtra(10).then(function(newPrice){
-    console.log(newPrice);
-}).catch(function(error){
-    console.log(error);
+// addExtra(10).then(function(newPrice){
+//     console.log(newPrice);
+// }).catch(function(error){
+//     console.log(error);
+// })
+
+//JS promises stand alone
+let price = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve(99.99);
+    }, 300);
 })
+
+price.then(function(price){
+    console.log(price);
+})
+
 
 // Classic callback
 // function addExtra(price, callback) {
