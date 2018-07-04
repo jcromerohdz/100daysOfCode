44f7cd25064dff915d354c7ac3c9021314e5d462
Christian <jcromerohdz@gmail.com>
2018-07-04 15:42:23 -0700

    Usign JS Fetch api

--- pwa/fetch.js
+++ pwa/fetch.js
@@ -1,26 +1,40 @@
 
 
+// JS Fetch
+
+fetch('https://api.githu.com/users/google/repos')
+     .then(function(response){
+         response.json().then(function(json){
+             console.log(json);
+         })
+     }).catch(function(error){
+        console.log('Fetch Error');
+        console.log(error)
+     });
+
+
+
 // Classic XHR
-if (window.XMLHttpRequest){
-    request = new XMLHttpRequest();
-} else if (window.ActiveXObject) {
-    try{
-        request = new ActiveXObject('Msxml2.XMLHTTP');
-    }
-    catch (e){
-        try{
-        request = new ActiveXObject('Microsoft.XMLHTTP');    
-       }
-       catch (e){}
-    }
-}
+// if (window.XMLHttpRequest){
+//     request = new XMLHttpRequest();
+// } else if (window.ActiveXObject) {
+//     try{
+//         request = new ActiveXObject('Msxml2.XMLHTTP');
+//     }
+//     catch (e){
+//         try{
+//         request = new ActiveXObject('Microsoft.XMLHTTP');    
+//        }
+//        catch (e){}
+//     }
+// }
 
-//Opend, send.
-request.open('GET', 'https://api.github.com/users/google/repos', true);
-request.send(null);
+// //Opend, send.
+// request.open('GET', 'https://api.github.com/users/google/repos', true);
+// request.send(null);
 
-request.onreadystatechange = function(state){
-    if(request.readyState === 4){
-        console.log(JSON.parse(request.response));
-    }
-}
+// request.onreadystatechange = function(state){
+//     if(request.readyState === 4){
+//         console.log(JSON.parse(request.response));
+//     }
+// }
