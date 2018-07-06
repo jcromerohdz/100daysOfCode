2723822837455ab1a63100b689fa133699a40923
jcromerohdz <jcromerohdz@gmail.com>
2018-07-05 18:37:18 -0700

    Node JS fundametals with third party module Yargs

--- NodeJS/notes-node/app.js
+++ NodeJS/notes-node/app.js
@@ -3,21 +3,29 @@ console.log('Staring app.js.');
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 const notes = require('./notes.js');
 
 
 //Getting input from the user
+const argv = yargs.argv;
 let command = process.argv[2]
 console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log("Yargs", argv);
 
 if (command === "add") {
   console.log("Adding new note");
+  notes.addNote(argv.title, argv.body);
 } else if (command === "list") {
   console.log("Listing all notes");
+  notes.getAll();
 } else if (command == "read") {
   console.log("Reading the note");
+  notes.getNote(argv.title);
 } else if (command == "remove") {
   console.log("Remove the note");
+  notes.removeNote(argv.title);
 } else{
     console.log("Command not recognized");
 }
--- NodeJS/notes-node/notes.js
+++ NodeJS/notes-node/notes.js
@@ -2,13 +2,36 @@ console.log("Starting notes.js");
 
 // console.log(module);
 
-module.exports.age = 39;
+// module.exports.age = 39;
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
-}
+// module.exports.addNote = () => {
+//     console.log('addNote');
+//     return 'New note';
+// }
 
-module.exports.add = (a, b) => {
-    return a + b;
-}
+// module.exports.add = (a, b) => {
+//     return a + b;
+// }
+
+let addNote = (title, body) =>{
+  console.log("Adding note", title, body);  
+};
+
+let getAll = () => {
+  console.log("Getting all notes");
+};
+
+let getNote = (title) => {
+  console.log("Getting note: ", title);
+};
+
+let removeNote = (title) => {
+  console.log("Removing note: ", title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
